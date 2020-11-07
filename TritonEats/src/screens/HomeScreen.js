a7a136cd856d4eb1e0c6b24935613bcb373ff2a2
Steven Wu <steven774@gmail.com>
2020-11-07 10:00:54 -0800

    Homepage (#8)
    * homepage basic layout with navbar
    
    * routed pages, working on landing page, imported fonts, imported logo
    
    * testing
    
    * .
    
    * testing
    
    * Landing screen signup/signin button frontend setup
    
    * fixed PR
    
    * fixed PR
    
    Co-authored-by: Steven Hsuante Wu <stevenwu@Stevens-MacBook-Air.local>
    Co-authored-by: Wei Thang Chai <weithangchai@WeiThangs-MacBook-Pro.local>

--- TritonEats/src/screens/HomeScreen.js
+++ TritonEats/src/screens/HomeScreen.js
@@ -1,16 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import { Text, View, StyleSheet } from "react-native";
 
-const HomeScreen = () => {
-  return (
-    <View>
-      <Text style={styles.text}>This is the home page text change page</Text>
-      <Text>Test</Text>
-    </View>
-  );
-};
+import NavBar from "../Components/NavBar";
+
+class HomeScreen extends Component {
+  render() {
+    return (
+      <View style={styles.main}>
+        <Text style={styles.text}>This is the main page</Text>
+        <Text style={styles.text}>This is body</Text>
+        <NavBar />
+      </View>
+    );
+  }
+}
 
 const styles = StyleSheet.create({
+  main: {
+    alignItems: "center",
+    flex: 1,
+    justifyContent: "space-between",
+  },
   text: {
     fontSize: 30,
   },
---
+++ TritonEats/src/screens/OrderHistoryScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+const OrderHistoryScreen = () => {
+  return <Text style={{ fontSize: 49 }}>OrderHistoryScreen</Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default OrderHistoryScreen;
