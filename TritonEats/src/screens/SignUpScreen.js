428380d4c0d9fd9a45e54efd70d1106be0ed1794
Steven H Wu <s2wu@ucsd.edu>
2020-11-13 15:24:59 -0800

    set up express api routes + started sign up screen authentication + about to start react routing authentication

--- TritonEats/src/screens/SignUpScreen.js
+++ TritonEats/src/screens/SignUpScreen.js
@@ -1,18 +1,56 @@
-import React from "react";
-import { View, StyleSheet, Text, Button } from "react-native";
+import React, { useState } from "react";
+import { View, StyleSheet, Text, Button, TextInput } from "react-native";
 
 const SignUpScreen = ({ navigation }) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   return (
     <>
-      <Text style={{ fontSize: 49 }}>SignUpScreen</Text>
-      <Button
-        title="Go to MainFlow"
-        onPress={() => navigation.navigate("mainFlow")}
-      />
+      <View style={styles.container}>
+        <Text>Sign Up Screen</Text>
+        <TextInput
+          label="Email"
+          value={email}
+          onChangeText={setEmail}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <TextInput
+          secureTextEntry
+          label="Password"
+          value={password}
+          onChangeText={setPassword}
+          autoCapitalize="none"
+          autoCorrect={false}
+        />
+        <Button title="Sign Up" />
+      </View>
     </>
   );
 };
 
-const styles = StyleSheet.create({});
+SignUpScreen.navigationOptions = () => {
+  return {
+    header: () => false,
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 250,
+  },
+});
 
 export default SignUpScreen;
+
+{
+  /* <>
+  <Text style={{ fontSize: 49 }}>SignUpScreen</Text>
+  <Button
+    title="Go to MainFlow"
+    onPress={() => navigation.navigate("mainFlow")}
+  />
+</>; */
+}
