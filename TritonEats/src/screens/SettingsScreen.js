028f661fd81f485cf420adb0c98e5fa23fa86e1e
Mudit-977 <mubajaj@ucsd.edu>
2020-11-13 19:16:25 +0530

    settings screen

--- TritonEats/src/screens/SettingsScreen.js
+++ TritonEats/src/screens/SettingsScreen.js
@@ -1,15 +1,41 @@
 import React from "react";
-import { View, StyleSheet, Text, Button } from "react-native";
+import { View, StyleSheet, Text, Button, FlatList } from "react-native";
 
 const SettingsScreen = ({ navigation }) => {
+
+  const menu = [{key: 'My Profile', nav: 'HomeScreen'}, {key: 'Payment Methods', nav: 'OrderHistoryScreen'}, {key: 'Manage Address', nav: 'ShoppingCartScreen'}, {key: 'Manage Profile Picture', nav: 'HomeScreen'}, {key: 'Change Password', nav: 'HomeScreen'}];
+
   return (
     <>
-      <Text style={{ fontSize: 49 }}>SettingsScreen</Text>
+      <Text style={{ fontSize: 49, padding: 15, paddingTop: 25 }}>Settings</Text>
+      
+      <FlatList style={ styles.list }
+      data={menu} renderItem={({item}) => 
+        <Text style={styles.text} onPress={() => navigation.navigate(item.nav)} >{item.key}</Text>
+      } />
+      
       <Button title="LogOut" onPress={() => navigation.navigate("loginFlow")} />
     </>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 20,
+    padding: 10,
+    paddingLeft: 15,
+    backgroundColor: '#0a2657',
+    color: '#FFD700',
+    marginBottom: 1
+    /*
+    borderTopWidth: 1,
+    borderTopColor: '#000000',
+    borderBottomWidth: 1,
+    borderBottomColor: '#000000'*/
+  },
+  list: {
+    paddingTop: 30
+  }
+});
 
 export default SettingsScreen;
