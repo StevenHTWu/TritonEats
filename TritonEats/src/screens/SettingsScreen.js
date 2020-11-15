1022bfdea3636204bed610e05dae11b12abf2a54
Mudit-977 <mubajaj@ucsd.edu>
2020-11-15 13:29:48 +0530

    settings

--- TritonEats/src/screens/SettingsScreen.js
+++ TritonEats/src/screens/SettingsScreen.js
@@ -3,20 +3,25 @@ import { View, StyleSheet, Text, Button, FlatList } from "react-native";
 
 const SettingsScreen = ({ navigation }) => {
 
-  const menu = [{key: 'My Profile', nav: 'HomeScreen'}, {key: 'Payment Methods', nav: 'OrderHistoryScreen'}, {key: 'Manage Address', nav: 'ShoppingCartScreen'}, {key: 'Manage Profile Picture', nav: 'HomeScreen'}, {key: 'Change Password', nav: 'HomeScreen'}];
+  const menu = [{key: 'My Profile', nav: 'HomeScreen'}, {key: 'Payment Methods', nav: 'OrderHistoryScreen'}, 
+  {key: 'Manage Address', nav: 'ShoppingCartScreen'}, {key: 'Manage Profile Picture', nav: 'HomeScreen'}, 
+  {key: 'Change Password', nav: 'HomeScreen'}, {key: 'Log Out', nav: 'loginFlow'}];
 
   return (
     <>
-      <Text style={{ fontSize: 49, padding: 15, paddingTop: 25 }}>Settings</Text>
+      <Text style={{ fontSize: 49, paddingTop: 40, fontFamily: "Unica One", textAlign: "center" }}>Settings</Text>
       
       <FlatList style={ styles.list }
       data={menu} renderItem={({item}) => 
         <Text style={styles.text} onPress={() => navigation.navigate(item.nav)} >{item.key}</Text>
       } />
       
-      <Button title="LogOut" onPress={() => navigation.navigate("loginFlow")} />
+      
     </>
   );
+  /*
+      <Button title="Log Out" onPress={() => navigation.navigate("loginFlow")} />
+*/
 };
 
 const styles = StyleSheet.create({
@@ -26,7 +31,7 @@ const styles = StyleSheet.create({
     paddingLeft: 15,
     backgroundColor: '#0a2657',
     color: '#FFD700',
-    marginBottom: 1
+    marginBottom: 1,
     /*
     borderTopWidth: 1,
     borderTopColor: '#000000',
@@ -34,7 +39,7 @@ const styles = StyleSheet.create({
     borderBottomColor: '#000000'*/
   },
   list: {
-    paddingTop: 30
+    paddingTop: 40
   }
 });
 
