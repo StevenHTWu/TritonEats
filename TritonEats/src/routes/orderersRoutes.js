dd31bcbed6879c36a9a17a3d9ba64c5be0d58bac
Michael Than <mythan@ucsd.edu>
2020-11-20 23:50:04 -0800

    Manually Merged Wei's API's into the code.

--- TritonEats/src/routes/orderersRoutes.js
+++ TritonEats/src/routes/orderersRoutes.js
@@ -1,17 +1,29 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const orderers = mongoose.model('orderers');
+const express = require("express");
+const mongoose = require("mongoose");
+const orderers = mongoose.model("orderers");
+const history = mongoose.model("history");
 
 const router = express.Router();
 
-router.route("/orderers").get(function(req, res) {
-    orderers.find({}, function(err, result) {
-      if (err) {
-        res.send(err);
-      } else {
-        res.json(result);
-      }
-    });
+router.route("/orderers").get(function (req, res) {
+  orderers.find({}, function (err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(result);
+    }
   });
+});
+
+router.route("/history/:orderer_id").get(function (req, res) {
+  orderer_id = req.params.orderer_id;
+  history.find({ orderer_id: orderer_id }, function (err, result) {
+    if (err) {
+      res.send(err);
+    } else {
+      res.json(result);
+    }
+  });
+});
 
 module.exports = router;
