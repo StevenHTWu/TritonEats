75af78015dbdd85e0e6355802ffe349f186010a6
Prabhjot Singh <prsingh@ucsd.edu>
2020-11-21 19:39:06 -0800

    Menu Screen Working

--- TritonEats/src/screens/MenuScreen.js
+++ TritonEats/src/screens/MenuScreen.js
@@ -6,7 +6,8 @@ import {
   FlatList,
   SafeAreaView,
   TouchableOpacity,
-  Button
+  Button,
+  Image
 } from "react-native";
 
 class MenuScreen extends Component {
@@ -15,18 +16,18 @@ class MenuScreen extends Component {
 
     this.state = {
       ResaturantMenu: [
-        { Id: "1", ItemName: "Sandwich", Price: "$7" },
+        { Id: "1", ItemName: "Triton Burger", Price: "$7" },
         { Id: "2", ItemName: "Noodles", Price: "$6.25" },
         { Id: "3", ItemName: "Pizza-Slice", Price: "$2.50" },
-        { Id: "4", ItemName: "Sandwich", Price: "$7" },
-        { Id: "5", ItemName: "Noodles", Price: "$6.25" },
-        { Id: "6", ItemName: "Pizza-Slice", Price: "$2.50" },
+        { Id: "4", ItemName: "California Roll", Price: "$9.00" },
+        { Id: "5", ItemName: "Spicy Tuna Roll", Price: "$9.00" },
+        { Id: "6", ItemName: "Tofu and Avocado Roll", Price: "$8.00" },
         { Id: "7", ItemName: "Sandwich", Price: "$7" },
-        { Id: "8", ItemName: "Noodles", Price: "$6.25" },
+        { Id: "8", ItemName: "Baja Fish Tacos", Price: "$6.00" },
         { Id: "9", ItemName: "Pizza-Slice", Price: "$2.50" },
         { Id: "10", ItemName: "Sandwich", Price: "$7" },
         { Id: "11", ItemName: "Noodles", Price: "$6.25" },
-        { Id: "12", ItemName: "Pizza-Slice", Price: "$2.50" },
+        { Id: "12", ItemName: "Pizza-Slice", Price: "$2.50" }
       ],
     };
   }
@@ -35,6 +36,7 @@ class MenuScreen extends Component {
     return (
       <SafeAreaView forceInset={{ top: "always" }}>
         <Text style={styles.headerTitle}>Pines</Text>
+        <Image style={styles.topImage} source={require('../../assets/Pines_Burger.jpg')} />
         <FlatList
           data={this.state.ResaturantMenu}
           renderItem={({ item }) => (
@@ -42,7 +44,7 @@ class MenuScreen extends Component {
                 <TouchableOpacity style={styles.borderItem}>
                     <Text style={styles.bodyText}>{item.ItemName}</Text>
                     <Text style={styles.priceText}>{item.Price}</Text>
-                    <View style={{marginLeft: 300, borderWidth: 1, marginBottom:10, borderColor: "#FFD700", borderRadius: 10}}>
+                    <View style={styles.addToCartButton}>
                         <Button
                             title="Add to Cart"
                             color="#FFD700"
@@ -68,22 +70,27 @@ const styles = StyleSheet.create({
   text: {
     fontSize: 30,
   },
+  topImage: {
+    marginBottom: 10,
+    width: 500,
+    height: 200,
+  },
   headerTitle: {
     fontSize: 49,
-    paddingTop: 40,
-    paddingBottom: 80,
+    paddingTop: 10,
+    paddingBottom: 10,
     fontFamily: "Unica One",
     textAlign: "center",
   },
   bodyText: {
-    fontSize: 45,
+    fontSize: 38,
     paddingTop: 5,
     color:"#FFD700",
     fontFamily: "Unica One",
     textAlign: "left",
   },
   priceText: {
-    fontSize: 24,
+    fontSize: 30,
     color:"#FFD700",
     paddingTop: 5,
     fontFamily: "Unica One",
@@ -94,6 +101,13 @@ const styles = StyleSheet.create({
     borderWidth: 0,
     backgroundColor:"#0a2657"
   },
+  addToCartButton:{
+    marginLeft: 300,
+    borderWidth: 1,
+    marginBottom:10,
+    borderColor: "#FFD700",
+    borderRadius: 10
+  }
 });
 
 export default MenuScreen;
