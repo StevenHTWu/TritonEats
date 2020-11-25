480eefb7fc784c7643a7a0761116309d895b62e7
Michael Than <mythan@ucsd.edu>
2020-11-25 15:55:31 -0800

    makeOrder done

--- TritonEats/src/routes/historyRoutes.js
+++ TritonEats/src/routes/historyRoutes.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const history = mongoose.model("history");
 
 const router = express.Router();
 
-router.route("/orderers").get(function (req, res) {
-  orderers.find({}, function (err, result) {
+router.route("/history/:orderer_id").get(function (req, res) {
+  orderer_id = req.params.orderer_id;
+  history.find({ orderer_id: orderer_id }, function (err, result) {
     if (err) {
       res.send(err);
     } else {
