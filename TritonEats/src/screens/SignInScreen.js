c35b603657f652d565b7b3cad0339ebc9b6d8003
PrabhjotSingh9 <prsingh@ucsd.edu>
2020-12-04 22:53:47 -0800

    Added back the SignIn/SignUp Screens

---
+++ TritonEats/src/screens/SignInScreen.js
@@ -0,0 +1,43 @@
+import React, { useContext } from "react";
+import { View, StyleSheet, Text, Button } from "react-native";
+import { NavigationEvents } from "react-navigation";
+import AuthForm from "../Components/AuthForm";
+import NavLink from "../Components/NavLink";
+import { Context } from "../context/AuthContext";
+
+const SignInScreen = ({ navigation }) => {
+  const { state, signin, clearErrorMessage } = useContext(Context);
+
+  return (
+    <View style={styles.container}>
+      <NavigationEvents onWillFocus={clearErrorMessage} />
+      <AuthForm
+        headerText="Sign In"
+        errorMessage={state.errorMessage}
+        onSubmit={signin}
+        deliverCheck=""
+        submitButtonText="Sign In"
+      />
+      <NavLink
+        text="Don't have an account? Sign up instead"
+        routeName="SignUpScreen"
+      />
+    </View>
+  );
+};
+
+SignInScreen.navigationOptions = () => {
+  return {
+    header: false,
+  };
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    marginBottom: 250,
+  },
+});
+
+export default SignInScreen;
