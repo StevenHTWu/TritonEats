30d0f7f1b28daea32bcebe7e9ef36a38dfa8270a
Kabir Nagrecha <knagrecha@kabirs-mbp.lan>
2020-12-07 12:21:28 -0800

    changed button font size

--- TritonEats/src/screens/DelivererStatusScreen.js
+++ TritonEats/src/screens/DelivererStatusScreen.js
@@ -271,7 +271,7 @@ const styles = StyleSheet.create({
     marginTop: "10%",
   },
   ButtonText: {
-    fontSize: 46,
+    fontSize: 34,
     color: "#FFD700",
     fontWeight: "bold",
     alignSelf: "center",
